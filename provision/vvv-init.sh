af173035f82991bc37cf38ab7569bb1b15233af1
Chris Scott <chris@iamzed.com>
2018-08-10 18:12:46 -0400

    redder

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -6,7 +6,7 @@ VIP_REPO=`get_config_value 'vip-repo'`
 # Check to make sure there's a VIP repo set in the config. We need this since 
 # it is the wp-content directory and we need to add mu-plugins after cloning it.
 if [ -z "$VIP_REPO" ]; then
-  echo -e "\e[31mVIP: vip-repo must be set in vvv-custom.yml. See [url to readme] for details. Exiting VIP site init..."
+  echo "VIP: vip-repo must be set in vvv-custom.yml. See [url to readme] for details. Exiting VIP site init..." > /dev/stderr
   exit 1
 fi
 
